68c5bb7bdd15ed613aed135b2b81c8c489aba9e0
Dibyendu <Dibyendu@LAPTOP-E3SSMVS9>
2023-03-27 14:45:13 +0530

    Constructor and it type

---
+++ Constructor and it types/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Constructor_and_it_types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
